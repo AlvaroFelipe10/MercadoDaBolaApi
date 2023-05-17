63878880cd8d5a939288107268c500aedca8f268
AlvaroFelipe10 <alvs.felipe@hotmail.com>
2023-05-17 14:36:52 -0300

    mudando relacionamentos

--- src/main/java/com/mercadodabola/mercadotransferencia/domain/entities/GolAssistPartidaEntity.java
+++ src/main/java/com/mercadodabola/mercadotransferencia/domain/entities/GolAssistPartidaEntity.java
@@ -1,17 +1,13 @@
 package com.mercadodabola.mercadotransferencia.domain.entities;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import javax.persistence.Embeddable;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.validation.Valid;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.mercadodabola.mercadotransferencia.domain.enums.TipoGolAssist;
@@ -32,10 +28,10 @@ public class GolAssistPartidaEntity {
 	
 	
 	@JsonIgnore
-	@OneToOne
-	private  PartidaEntity partida;
+	@ManyToOne(fetch=FetchType.LAZY)
+	private PartidaEntity partida;
 	
-	@OneToOne
+	@ManyToOne
 	@JoinColumn(name = "jogador_id", nullable = false)
 	private JogadorEntity jogadorId;
 	
